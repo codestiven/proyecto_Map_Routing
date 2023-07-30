a9db5323dee66a834c63adf284fed81415b6177c
stiven de la rosa brito <codestiven@gmail.com>
2023-07-29 22:18:29 -0400

    inicialización de las clases

--- importar_osm.py
+++ importar_osm.py
@@ -3,3 +3,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class OSMHandler(osmium.SimpleHandler):
+    def __init__(self, graph):
+        super(OSMHandler, self).__init__()
+        self.graph = graph
+        self.added_nodes = set()
+        self.multiple_edges_nodes = []
+
+   
